4511090b4386b335b8d7171c2937a456bd24bada
Daniel667Liu <92137425+Daniel667Liu@users.noreply.github.com>
2022-11-13 16:01:22 -0500

    1113_Add back to Main Menu, Quit Game

--- Assets/Danny/Scripts/NavgationUI.cs
+++ Assets/Danny/Scripts/NavgationUI.cs
@@ -7,6 +7,7 @@ public class NavgationUI : MonoBehaviour
     // Start is called before the first frame update
     public GameObject insideUI;
     public GameObject hamburgerUI;
+    public GameObject backUI;
     void Start()
     {
         insideUI.SetActive(false);
@@ -22,11 +23,24 @@ public class NavgationUI : MonoBehaviour
     {
         insideUI.SetActive(true);
         hamburgerUI.SetActive(false);
+        if (backUI.activeInHierarchy) 
+        {
+            backUI.SetActive(false);
+        }
     }
 
     public void CloseUI() 
     {
         insideUI.SetActive(false);
         hamburgerUI.SetActive(true);
+        if (!backUI.activeInHierarchy)
+        {
+            backUI.SetActive(true);
+        }
+    }
+
+    public void CloseUIMain() 
+    {
+        insideUI.SetActive(false);
     }
 }
