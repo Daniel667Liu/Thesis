c3712733a0a85e42d01f985b55abc2d5f9ff1c30
Daniel667Liu <92137425+Daniel667Liu@users.noreply.github.com>
2022-11-20 20:54:42 -0500

    1120_WIP

--- Assets/Danny/Scripts/FireflyControl/FireFlyControl.cs
+++ Assets/Danny/Scripts/FireflyControl/FireFlyControl.cs
@@ -6,24 +6,28 @@ public class FireFlyControl : MonoBehaviour
 {
     public List<GameObject> flies;
     List<Vector3> DePosition;
+    List<Vector3> desPoses;
     public float noiseScalarX = 0f;
     public float noiseScalarY = 0f;
     public float noiseScalarZ = 0f;
     public float speed = 1f;
     float noiseSampleCoord = 0f;
     Vector3 center;
-    bool moveable;
-
+    public bool moveable = true;
+    public float posUpdateGap = 0f;
+    public float timeGap = 1.5f;
 
     // Start is called before the first frame update
     void Start()
     {
         DePosition = new List<Vector3>();
+        desPoses = new List<Vector3>();
         foreach (GameObject fly in flies) 
         {
-            DePosition.Add(fly.transform.position);
+            DePosition.Add(fly.transform.localPosition);
         }
         GetCenterPos();
+        Debug.Log(center);
     }
 
     // Update is called once per frame
@@ -32,7 +36,9 @@ public class FireFlyControl : MonoBehaviour
         if (moveable) 
         {
             NoiseFly();
+            posUpdateGap += Time.deltaTime;
         }
+        
     }
 
     void NoiseFly() 
@@ -41,14 +47,24 @@ public class FireFlyControl : MonoBehaviour
         for (int i = 0; i < flies.Count; i++) 
         {
             Vector3 tarPos;
-            tarPos.x= center.x * (1f + (2f * Mathf.PerlinNoise(i + noiseSampleCoord, i + noiseSampleCoord) - 1f) * noiseScalarX);
-            tarPos.y = center.y * (1f + (2f * Mathf.PerlinNoise(i+0.5f + noiseSampleCoord, i + noiseSampleCoord) - 1f) * noiseScalarY);
-            tarPos.z = center.z * (1f + (2f * Mathf.PerlinNoise(i +1f+ noiseSampleCoord, i + noiseSampleCoord) - 1f) * noiseScalarZ);
+            tarPos.x= center.x  + (2f * Mathf.PerlinNoise(i + noiseSampleCoord, i + noiseSampleCoord) - 1f) * noiseScalarX;
+            tarPos.y = center.y + (2f * Mathf.PerlinNoise(i+0.5f + noiseSampleCoord, i + noiseSampleCoord) - 1f) * noiseScalarY;
+            tarPos.z = center.z + (2f * Mathf.PerlinNoise(i +1f+ noiseSampleCoord, i + noiseSampleCoord) - 1f) * noiseScalarZ;
 
             Vector3 desPos = Vector3.MoveTowards(DePosition[i], tarPos,1f);
-            flies[i].transform.position = desPos;
+            //Debug.Log(tarPos);
+            desPoses.Add(tarPos);
+            //flies[i].transform.localPosition = desPos;
         }
         noiseSampleCoord += Time.deltaTime*speed;
+        if (posUpdateGap >= timeGap) 
+        {
+            for (int i = 0; i < flies.Count; i++) 
+            {
+                flies[i].transform.localPosition = desPoses[i];
+            }
+            posUpdateGap = 0f;
+        }
     }
 
     void GetCenterPos() 
@@ -57,7 +73,9 @@ public class FireFlyControl : MonoBehaviour
         for (int i = 1; i < DePosition.Count; i++)
         {
             center = (center + DePosition[i]) * 0.5f;
+            Debug.Log(center);
         }
+        
     }
 
     public void StartMoving() 
