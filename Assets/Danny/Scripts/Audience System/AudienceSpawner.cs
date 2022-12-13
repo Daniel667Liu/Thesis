c9d4c50488028e9dedf826bb6a91372acec9030b
Daniel667Liu <92137425+Daniel667Liu@users.noreply.github.com>
2022-12-13 16:15:47 -0500

    1213_Daniel_Audience system finish

--- Assets/Danny/Scripts/Audience System/AudienceSpawner.cs
+++ Assets/Danny/Scripts/Audience System/AudienceSpawner.cs
@@ -9,19 +9,22 @@ public class AudienceSpawner : MonoBehaviour
     public GameObject box;
     //AudienceManager manager;
     int audienceIndex = 0;
+    [Tooltip("vector2 to store min wait time and max wait time for spawn a new audience.")]
+    public Vector2 gapTimeRange;
     // Start is called before the first frame update
     void Start()
     {
-        //manager = FindObjectOfType<AudienceManager>();
+        
+        StartCoroutine(SpawnCoroutine());
     }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.K)) 
-        {
-            SpawnAudience();
-        }
+            {
+                SpawnAudience();
+            }
     }
 
     public void SpawnAudience() 
@@ -41,5 +44,24 @@ public class AudienceSpawner : MonoBehaviour
         }
     }
 
-    
+    IEnumerator SpawnCoroutine() 
+    {
+        while (true) 
+        {
+            yield return new WaitForEndOfFrame();
+            SpawnAudience();
+            yield return new WaitForSeconds(Random.Range(gapTimeRange.x,gapTimeRange.y));
+        }
+        
+            
+    }
+    private void OnDisable()
+    {
+        StopAllCoroutines();
+    }
+
+    private void OnDestroy()
+    {
+        StopAllCoroutines();
+    }
 }
