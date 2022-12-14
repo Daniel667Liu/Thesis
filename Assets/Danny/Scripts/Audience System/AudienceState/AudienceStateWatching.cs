38d22be859c47ddf0b53ac633bbbd1c6179b57d3
Daniel667Liu <92137425+Daniel667Liu@users.noreply.github.com>
2022-12-14 16:15:51 -0500

    1214_Daniel_Audience revised.

--- Assets/Danny/Scripts/Audience System/AudienceState/AudienceStateWatching.cs
+++ Assets/Danny/Scripts/Audience System/AudienceState/AudienceStateWatching.cs
@@ -7,12 +7,13 @@ public class AudienceStateWatching : AudienceStateBase
     public override void EnterState(Audience audience)
     {
         audience.animator.SetTrigger("Watch");
+        audience.isWatched = true;
     }
     public override void UpdateState(Audience audience)
     {
         if (Services.audienceManager.attraction < audience.data.leaveThres) 
         {
-            audience.ToLeaveState();
+            audience.ToWalkState();
         }
     }
 
